171c3c87d7f2a947da576bdadd3ce3e2e1a5fca9
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-11-26 17:20:02 +0100

    Bugfix

--- install.sql
+++ install.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%markitup_profiles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `%TABLE_PREFIX%markitup_profiles` (`id`, `name`, `description`, `language`, `type`, `markitup_buttons`) VALUES
-(1, 'full', 'Standard MarkItUp-Konfiguration', 'de', 'textile', 'bold,deleted,formatting[h1h2|h3|h4|h5|h6|p],italic,orderedlist,underline,unorderedlist');
+(1, 'full', 'Standard MarkItUp-Konfiguration', 'de', 'textile', 'bold,deleted,formatting[h1|h2|h3|h4|h5|h6|p],italic,orderedlist,underline,unorderedlist');
 
 ALTER TABLE `%TABLE_PREFIX%markitup_profiles`
  ADD PRIMARY KEY (`id`);
