c535ffc9c4aa22caeef9e43f7b1f7c0def9d2c75
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-12-01 16:05:54 +0100

    Buttons Code, Bild, Link und Zitat hinzugefügt

--- install.sql
+++ install.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%markitup_profiles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `%TABLE_PREFIX%markitup_profiles` (`id`, `name`, `description`, `type`, `markitup_buttons`) VALUES
-(1, 'full', 'Standard MarkItUp-Konfiguration', 'textile', 'bold,deleted,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],formatting[h1|h2|h3|h4|h5|h6|p],italic,orderedlist,sub,sup,table,underline,unorderedlist');
+(1, 'full', 'Standard MarkItUp-Konfiguration', 'textile', 'bold,code,deleted,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],formatting[h1|h2|h3|h4|h5|h6|p],image,italic,link[internal|external|mailto],orderedlist,quote,sub,sup,table,underline,unorderedlist');
 
 ALTER TABLE `%TABLE_PREFIX%markitup_profiles`
  ADD PRIMARY KEY (`id`);
