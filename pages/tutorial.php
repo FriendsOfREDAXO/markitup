93f58ebc47cb4adc463cddb97a3d6805642c8af8
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-12-04 10:00:41 +0100

    ModuleInput angepasst gemäss offizieller Vorgabe

--- pages/tutorial.php
+++ pages/tutorial.php
@@ -2,11 +2,13 @@
 	echo rex_view::info($this->i18n('tutorial_infotext'));
 	
 	$moduleInput = '';
-	$moduleInput .= '<fieldset>'.PHP_EOL;
-	$moduleInput .= '  <dl class="rex-form-group form-group">'.PHP_EOL;
-	$moduleInput .= '    <dt><label for="markitup_1">VALUE 1:</label></dt>'.PHP_EOL;
-	$moduleInput .= '    <dd><textarea cols="1" rows="6" class="form-control markitupEditor-full" id="markitup_1" name="REX_INPUT_VALUE[1]">REX_VALUE[1]</textarea></dd>'.PHP_EOL;
-	$moduleInput .= '  </dl>'.PHP_EOL;
+	$moduleInput .= '<fieldset class="form-horizontal">'.PHP_EOL;
+	$moduleInput .= '  <div class="form-group">'.PHP_EOL;
+	$moduleInput .= '    <label class="col-sm-2 control-label" for="markitup_1">VALUE 1</label>'.PHP_EOL;
+	$moduleInput .= '    <div class="col-sm-10">'.PHP_EOL;
+	$moduleInput .= '      <textarea cols="1" rows="6" class="form-control markitupEditor-full" id="markitup_1" name="REX_INPUT_VALUE[1]">REX_VALUE[1]</textarea>'.PHP_EOL;
+	$moduleInput .= '    </div>'.PHP_EOL;
+	$moduleInput .= '  </div>'.PHP_EOL;
 	$moduleInput .= '</fieldset>'.PHP_EOL;
 	
 	$fragment = new rex_fragment();
