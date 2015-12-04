009ff4ea493cd49e4051028ddb475089effa1dc1
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-12-04 16:43:42 +0100

    Erweitert mit neuem Beispiel

--- pages/tutorial.php
+++ pages/tutorial.php
@@ -1,19 +1,32 @@
 <?php
 	echo rex_view::info($this->i18n('tutorial_infotext'));
 	
-	$moduleInput = '';
-	$moduleInput .= '<fieldset class="form-horizontal">'.PHP_EOL;
-	$moduleInput .= '  <div class="form-group">'.PHP_EOL;
-	$moduleInput .= '    <label class="col-sm-2 control-label" for="markitup_1">VALUE 1</label>'.PHP_EOL;
-	$moduleInput .= '    <div class="col-sm-10">'.PHP_EOL;
-	$moduleInput .= '      <textarea cols="1" rows="6" class="form-control markitupEditor-full" id="markitup_1" name="REX_INPUT_VALUE[1]">REX_VALUE[1]</textarea>'.PHP_EOL;
-	$moduleInput .= '    </div>'.PHP_EOL;
-	$moduleInput .= '  </div>'.PHP_EOL;
-	$moduleInput .= '</fieldset>'.PHP_EOL;
+	$code = '';
+	$code .= '<fieldset class="form-horizontal">'.PHP_EOL;
+	$code .= '  <div class="form-group">'.PHP_EOL;
+	$code .= '    <label class="col-sm-2 control-label" for="markitup_1">VALUE 1</label>'.PHP_EOL;
+	$code .= '    <div class="col-sm-10">'.PHP_EOL;
+	$code .= '      <textarea cols="1" rows="6" class="form-control markitupEditor-full" id="markitup_1" name="REX_INPUT_VALUE[1]">REX_VALUE[1]</textarea>'.PHP_EOL;
+	$code .= '    </div>'.PHP_EOL;
+	$code .= '  </div>'.PHP_EOL;
+	$code .= '</fieldset>'.PHP_EOL;
 	
 	$fragment = new rex_fragment();
 	$fragment->setVar('class', 'info', false);
 	$fragment->setVar('title', 'Beispiel: Module Input', false); //todo
-	$fragment->setVar('body', highlight_string($moduleInput, true), false);
+	$fragment->setVar('body', highlight_string($code, true), false);
+	echo $fragment->parse('core/page/section.php');
+	
+	///
+	
+	$code = '';
+	$code .= '<?php'.PHP_EOL;
+	$code .= '  rex_markitup::insertProfile(\'simple\', \'beschreibung\', \'textile\', \'bold,italic\');'.PHP_EOL;
+	$code .= '?>'.PHP_EOL;
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', 'Beispiel: Via Modul oder AddOn ein Profil anlegen', false); //todo
+	$fragment->setVar('body', highlight_string($code, true), false);
 	echo $fragment->parse('core/page/section.php');
 ?>
