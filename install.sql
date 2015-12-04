3ecf82f51553c4e0d1810c7d8e853438c91e1f2d
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-12-04 16:41:27 +0100

    Bugfix: AUTO_INCREMENT fehlte

--- install.sql
+++ install.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%markitup_profiles` (
   `description` varchar(255) NOT NULL DEFAULT '',
   `type` varchar(50) NOT NULL,
   `markitup_buttons` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 INSERT INTO `%TABLE_PREFIX%markitup_profiles` (`id`, `name`, `description`, `type`, `markitup_buttons`) VALUES
 (1, 'full', 'Standard MarkItUp-Konfiguration', 'textile', 'bold,code,deleted,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],formatting[h1|h2|h3|h4|h5|h6|p],image,italic,link[internal|external|mailto],orderedlist,quote,sub,sup,table,underline,unorderedlist');
