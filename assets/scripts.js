6950a95e028b6388c43a4e65733edbf380d58c7b
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2016-01-07 09:47:59 +0100

    Funktionen aufgeteilt in markdown + textile

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,96 +1,204 @@
-function btnImageCallback (h) {
-	var markitupFieldID = h.textarea.id;
-	newPoolWindow('index.php?page=mediapool/media&opener_input_field='+markitupFieldID);
-}
-
-function btnImageCallbackInsert (id, url, linktext) {
-	window.opener.$.markItUp({
-		target:'#'+id,
-		openWith:'!'+url+'('+linktext+')!'
-	});
-}
-
-function btnLinkExternalCallback (h) {
-	var linktext = h.selection;
-	if (linktext == '') {
-		var linktext = prompt('Linktext?');
+//Start - temporary functions to prevent errors
+	function btnImageCallbackInsert() {
+		alert('Module-Input is misconfigured, please use the example!');
 	}
 	
-	var linkurl = prompt('URL?');
-	
-	return '"'+linktext+'":'+linkurl;
-}
-
-function btnLinkInternalCallback (h) {
-	var markitupFieldID = h.textarea.id;
-	openLinkMap(markitupFieldID);
-}
-
-function btnLinkInternalCallbackInsert (id, url, linktext) {
-	window.opener.$.markItUp({
-		target:'#'+id,
-		openWith: '"',
-		closeWith: '('+linktext+')":'+url
-	});
-}
-
-function btnLinkMailtoCallback (h) {
-	var linktext = h.selection;
-	if (linktext == '') {
-		var linktext = prompt('Linktext?');
+	function btnLinkInternalCallbackInsert() {
+		alert('Module-Input is misconfigured, please use the example');
 	}
-  var emailaddress = prompt('Emailaddress?');
-  
-  return '"'+linktext+'":mailto:'+emailaddress;
-}
+//End - temporary functions to prevent errors
 
-function btnTableCallback (h) {
-	cols = prompt('Wie viele Spalten?');
-	rows = prompt('Wie viele Zeilen?');
-	html = '';
+//Start - functions for markdown
+	function btnMarkdownImageCallback (h) {
+		var markitupFieldID = h.textarea.id;
+		newPoolWindow('index.php?page=mediapool/media&opener_input_field='+markitupFieldID);
+	}
 	
-	for (r = 0; r < rows; r++) {
-		for (c = 0; c < cols; c++) {
-			html += '|ABC';
+	function btnMarkdownImageCallbackInsert (id, url, linktext) {
+		window.opener.$.markItUp({
+			target:'#'+id,
+			openWith:'['+linktext+']('+url+')'
+		});
+	}
+	
+	function btnMarkdownLinkExternalCallback (h) {
+		var linktext = h.selection;
+		if (linktext == '') {
+			var linktext = prompt('Linktext?'); //todo
 		}
-		html += '|\n';
+		
+		var linkurl = prompt('URL?'); //todo
+		
+		return '['+linktext+']('+linkurl+')';
 	}
 	
-	return html;
-}
-
-function btnOrderedlistCallback (h) {
-	var selection = h.selection;
+	function btnMarkdownLinkInternalCallback (h) {
+		var markitupFieldID = h.textarea.id;
+		openLinkMap(markitupFieldID);
+	}
 	
-	var lines = selection.split(/\r?\n/);
-	var r = "";
-	var start = "# ";
-	for (var i=0; i < lines.length; i++) {
-		line = lines[i];
-		
-		r = r + start + line;
-		
-		if (i != lines.length - 1) {
-			r += "\n";
+	function btnMarkdownLinkInternalCallbackInsert (id, url, linktext) {
+		window.opener.$.markItUp({
+			target:'#'+id,
+			openWith: '[',
+			closeWith: ']('+url+')'
+		});
+	}
+	
+	function btnMarkdownLinkMailtoCallback (h) {
+		var linktext = h.selection;
+		if (linktext == '') {
+			var linktext = prompt('Linktext?'); //todo
 		}
+		var emailaddress = prompt('Emailadresse?'); //todo
+		
+		return '['+linktext+'](mailto:'+emailaddress+')';
 	}
-	return r;
-}
-
-function btnUnorderedlistCallback (h) {
-	var selection = h.selection;
 	
-	var lines = selection.split(/\r?\n/);
-	var r = "";
-	var start = "* ";
-	for (var i=0; i < lines.length; i++) {
-		line = lines[i];
+	function btnMarkdownTableCallback (h) {
+		cols = prompt('Wie viele Spalten?'); //todo
+		rows = prompt('Wie viele Zeilen?'); //todo
+		html = '';
 		
-		r = r + start + line;
-		
-		if (i != lines.length - 1) {
-			r += "\n";
+		for (r = 0; r < rows; r++) {
+			for (c = 0; c < cols; c++) {
+				html += '| ABC ';
+			}
+			html += '|\n';
 		}
+		
+		return html;
 	}
-	return r;
-}
+	
+	function btnMarkdownOrderedlistCallback (h) {
+		var selection = h.selection;
+		
+		var lines = selection.split(/\r?\n/);
+		var r = "";
+		for (var i=0; i < lines.length; i++) {
+			line = lines[i];
+			
+			r = r + (i+1) + '. ' + line;
+			
+			if (i != lines.length - 1) {
+				r += "\n";
+			}
+		}
+		return r;
+	}
+	
+	function btnMarkdownUnorderedlistCallback (h) {
+		var selection = h.selection;
+		
+		var lines = selection.split(/\r?\n/);
+		var r = "";
+		for (var i=0; i < lines.length; i++) {
+			line = lines[i];
+			
+			r = r + "- " + line;
+			
+			if (i != lines.length - 1) {
+				r += "\n";
+			}
+		}
+		return r;
+	}
+//End - functions for markdown
+
+
+//Start - functions for textile
+	function btnTextileImageCallback (h) {
+		var markitupFieldID = h.textarea.id;
+		newPoolWindow('index.php?page=mediapool/media&opener_input_field='+markitupFieldID);
+	}
+	
+	function btnTextileImageCallbackInsert (id, url, linktext) {
+		window.opener.$.markItUp({
+			target:'#'+id,
+			openWith:'!'+url+'('+linktext+')!'
+		});
+	}
+	
+	function btnTextileLinkExternalCallback (h) {
+		var linktext = h.selection;
+		if (linktext == '') {
+			var linktext = prompt('Linktext?'); //todo
+		}
+		
+		var linkurl = prompt('URL?'); //todo
+		
+		return '"'+linktext+'":'+linkurl;
+	}
+	
+	function btnTextileLinkInternalCallback (h) {
+		var markitupFieldID = h.textarea.id;
+		openLinkMap(markitupFieldID);
+	}
+	
+	function btnTextileLinkInternalCallbackInsert (id, url, linktext) {
+		window.opener.$.markItUp({
+			target:'#'+id,
+			openWith: '"',
+			closeWith: '('+linktext+')":'+url
+		});
+	}
+	
+	function btnTextileLinkMailtoCallback (h) {
+		var linktext = h.selection;
+		if (linktext == '') {
+			var linktext = prompt('Linktext?'); //todo
+		}
+		var emailaddress = prompt('Emailadresse?'); //todo
+		
+		return '"'+linktext+'":mailto:'+emailaddress;
+	}
+
+	function btnTextileTableCallback (h) {
+		cols = prompt('Wie viele Spalten?'); //todo
+		rows = prompt('Wie viele Zeilen?'); //todo
+		html = '';
+		
+		for (r = 0; r < rows; r++) {
+			for (c = 0; c < cols; c++) {
+				html += '|ABC';
+			}
+			html += '|\n';
+		}
+		
+		return html;
+	}
+	
+	function btnTextileOrderedlistCallback (h) {
+		var selection = h.selection;
+		
+		var lines = selection.split(/\r?\n/);
+		var r = "";
+		for (var i=0; i < lines.length; i++) {
+			line = lines[i];
+			
+			r = r + "# " + line;
+			
+			if (i != lines.length - 1) {
+				r += "\n";
+			}
+		}
+		return r;
+	}
+	
+	function btnTextileUnorderedlistCallback (h) {
+		var selection = h.selection;
+		
+		var lines = selection.split(/\r?\n/);
+		var r = "";
+		for (var i=0; i < lines.length; i++) {
+			line = lines[i];
+			
+			r = r + "* " + line;
+			
+			if (i != lines.length - 1) {
+				r += "\n";
+			}
+		}
+		return r;
+	}
+//End - functions for textile
