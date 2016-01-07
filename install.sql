269ab260de8d97c666107699cd241c444e868d7f
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2016-01-07 09:48:47 +0100

    Beispielprofil für markdown eingefügt

--- install.sql
+++ install.sql
@@ -9,10 +9,11 @@ CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%markitup_profiles` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 INSERT INTO `%TABLE_PREFIX%markitup_profiles` (`id`, `name`, `description`, `type`, `markitup_buttons`) VALUES
-(1, 'full', 'Standard MarkItUp-Konfiguration', 'textile', 'bold,code,deleted,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],heading1,heading2,heading3,heading4,heading5,heading6,image,italic,link[internal|external|mailto],orderedlist,paragraph,quote,sub,sup,table,underline,unorderedlist');
+(1, 'textile_full', 'Standard MarkItUp-Konfiguration', 'textile', 'bold,code,deleted,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],heading1,heading2,heading3,heading4,heading5,heading6,image,italic,link[internal|external|mailto],orderedlist,paragraph,quote,sub,sup,table,underline,unorderedlist'),
+(2, 'markdown_full', 'Standard MarkItUp-Konfiguration', 'markdown', 'bold,code,deleted,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],heading1,heading2,heading3,heading4,heading5,heading6,image,italic,link[internal|external|mailto],orderedlist,paragraph,quote,sub,sup,table,underline,unorderedlist');
 
 ALTER TABLE `%TABLE_PREFIX%markitup_profiles`
  ADD PRIMARY KEY (`id`);
  
 ALTER TABLE `%TABLE_PREFIX%markitup_profiles`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
