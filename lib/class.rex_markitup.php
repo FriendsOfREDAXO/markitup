762bab7bdc1410639c955ec6c2363d1862d11fb3
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2016-01-29 15:40:28 +0100

    Methode 'parseOutput' hinzugefügt

--- lib/class.rex_markitup.php
+++ lib/class.rex_markitup.php
@@ -1,4 +1,9 @@
 <?php
+	require_once (rex_path::addon('rex_markitup','vendor/php-textile/src/Netcarver/Textile/Parser.php')); //todo: use $this
+	require_once (rex_path::addon('rex_markitup','vendor/php-textile/src/Netcarver/Textile/DataBag.php')); //todo: use $this
+	require_once (rex_path::addon('rex_markitup','vendor/php-textile/src/Netcarver/Textile/Tag.php')); //todo: use $this
+	require_once (rex_path::addon('rex_markitup','vendor/parsedown/Parsedown.php')); //todo: use $this
+	
 	class rex_markitup {
 		
 		public static function insertProfile ($name, $description = '', $type = '', $markitupButtons = '') {
@@ -27,6 +32,23 @@
 			} else {
 				return false;
 			}
+		}
+		
+		public static function parseOutput ($type, $content) {
+			$content = str_replace('<br />', '', $content);
+			
+			switch ($type) {
+				case 'markdown':
+					$parser = new Parsedown();
+					return $parser->text($content);
+				break;
+				case 'textile':
+					$parser = new \Netcarver\Textile\Parser();
+					return $parser->parse($content);
+				break;
+			}
+			
+			return false;
 		}
 	}
 ?>
