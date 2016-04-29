96eb3b7e4387768c9783a5b0441947cad1c6eb77
Thomas Kaegi <tk@meeloo.ch>
2016-04-29 14:48:53 +0200

    Fixes #26

--- lib/class.rex_markitup_markdown.php
+++ lib/class.rex_markitup_markdown.php
@@ -1,5 +1,7 @@
 <?php
-	require_once (rex_path::addon('rex_markitup','vendor/parsedown/Parsedown.php')); //todo: use $this
+	if (!class_exists('Parsedown')) {
+		require_once (rex_path::addon('rex_markitup','vendor/parsedown/Parsedown.php')); //todo: use $this
+	}
 	
 	class rex_markitup_markdown extends Parsedown {
 		
--- lib/class.rex_markitup_textile.php
+++ lib/class.rex_markitup_textile.php
@@ -1,7 +1,9 @@
 <?php
-	require_once (rex_path::addon('rex_markitup','vendor/php-textile/src/Netcarver/Textile/Parser.php')); //todo: use $this
-	require_once (rex_path::addon('rex_markitup','vendor/php-textile/src/Netcarver/Textile/DataBag.php')); //todo: use $this
-	require_once (rex_path::addon('rex_markitup','vendor/php-textile/src/Netcarver/Textile/Tag.php')); //todo: use $this
+	if (!class_exists('Netcarver\Textile\Parser')) {
+		require_once (rex_path::addon('rex_markitup','vendor/php-textile/src/Netcarver/Textile/Parser.php')); //todo: use $this
+		require_once (rex_path::addon('rex_markitup','vendor/php-textile/src/Netcarver/Textile/DataBag.php')); //todo: use $this
+		require_once (rex_path::addon('rex_markitup','vendor/php-textile/src/Netcarver/Textile/Tag.php')); //todo: use $this
+	}
 	
 	class rex_markitup_textile extends Netcarver\Textile\Parser {
 		public function __construct($doctype = 'xhtml') {
