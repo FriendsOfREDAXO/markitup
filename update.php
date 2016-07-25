85cb2a67a4223687494117fcee63c41330ca784e
Thomas Kaegi <tk@meeloo.ch>
2016-07-25 10:38:00 +0200

    Fixes #43

--- update.php
+++ update.php
@@ -1,5 +1,6 @@
 <?php
 	rex_sql_table::get(rex::getTable('markitup_profiles'))
+	->ensureColumn(new rex_sql_column('urltype', 'varchar(50)'))
 	->ensureColumn(new rex_sql_column('minheight', 'smallint(5)'))
 	->ensureColumn(new rex_sql_column('maxheight', 'smallint(5)'))
 	->alter();
