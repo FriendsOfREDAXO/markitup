06f77b610b0a755f31067c03c2275e457f8bc7a9
christophboecker <christoph_boecker@t-online.de>
2018-05-25 19:29:52 +0200

    Generieren von markitup_profiles.css/js optimiert
    Die beiden Dateien wurden bisher bei jedem Click (egal wo im Backend) neu generiert ( siehe Issues #72 und #80). Der entsprechende Code ist nun an die Extension-Points REX_FORM_SAVED (add/edit) und REX_FORM_DELETED gekoppelt, wird also nur noch ausgeführt, wenn tatsächlich Änderungen vorliegen. Die eigentliche Code in nach `markitup::createCodeFromProfiles` ausgelagert.

--- boot.php
+++ boot.php
@@ -1,5 +1,30 @@
 <?php
 	if (rex::isBackend()) {
+
+        # Wenn ein Profil erfolgreich gespeichert wurde (neu oder als Update)
+        # bzw. wenn es gelöscht wurde, werden die darauf basierenden
+        # markitup_profiles.css/js neu angelegt
+
+        rex_extension::register( 'REX_FORM_SAVED', function( $ep ) {
+
+            if( strcasecmp( $ep->getParams()['form']->getTableName(),rex::getTable('markitup_profiles') ) == 0 )
+            {
+                markitup::createCodeFromProfiles( $this );
+            }
+
+        } );
+
+        rex_extension::register( 'REX_FORM_DELETED', function( $ep ) {
+
+            if( strcasecmp( $ep->getParams()['form']->getTableName(),rex::getTable('markitup_profiles') ) == 0 )
+            {
+                markitup::createCodeFromProfiles( $this );
+            }
+
+        } );
+
+        # Ressourcen einbinden
+
 		rex_view::addJsFile($this->getAssetsUrl('jquery.markitup.js'));
 		rex_view::addJsFile($this->getAssetsUrl('autosize.min.js'));
 		rex_view::addJsFile($this->getAssetsUrl('scripts.js'));
@@ -7,49 +32,7 @@
 		if (file_exists($this->getAssetsPath('skin.css'))) {
 			rex_view::addCssFile($this->getAssetsUrl('skin.css'));
 		}
-		
-		//Start - get markitup-profiles
-			$sql = rex_sql::factory();
-			$profiles = $sql->setQuery("SELECT `name`, `type`, `minheight`, `maxheight`, `markitup_buttons` FROM `".rex::getTablePrefix()."markitup_profiles` ORDER BY `name` ASC")->getArray();
-			unset($sql);
-			
-			$cssCode = [];
-			$jsCode = [];
-			
-			$jsCode[] = 'function markitupInit() {';
-			foreach ($profiles as $profile) {
-				$cssCode[] = '  textarea.markitupEditor-'.$profile['name'].' { min-height: '.$profile['minheight'].'px; max-height: '.$profile['maxheight'].'px; }';
-				$jsCode[] = '  $(\'.markitupEditor-'.$profile['name'].'\').not(\'.markitupActive\').markItUp({';
-				
-				$jsCode[] = '    nameSpace: "markitup_'.$profile['type'].'",';
-				switch ($profile['type']) {
-					case 'textile':
-						$jsCode[] = '    onShiftEnter: {keepDefault:false, replaceWith:\'\n\n\'},';
-					break;
-				}
-				
-				$jsCode[] = '    markupSet: [';
-				$jsCode[] = '      '.markitup::defineButtons($profile['type'], $profile['markitup_buttons'], $this);
-				$jsCode[] = '    ]';
-				$jsCode[] = '  }).addClass(\'markitupActive\');';
-			}
-			$jsCode[] = '}';
-			
-			$jsCode[] = '$(document).on(\'ready pjax:success\',function() {';
-			$jsCode[] = '  markitupInit();';
-			$jsCode[] = '  autosize($("textarea[class*=\'markitupEditor-\']"));';
-			$jsCode[] = '});';
-			
-			if (!rex_file::put(rex_path::addonAssets('markitup', 'cache/markitup_profiles.css').'', implode(PHP_EOL, $cssCode))) {
-				echo 'css-file konnte nicht gespeichert werden';
-			}
-			
-			if (!rex_file::put(rex_path::addonAssets('markitup', 'cache/markitup_profiles.js').'', implode(PHP_EOL, $jsCode))) {
-				echo 'js-file konnte nicht gespeichert werden';
-			}
-			
-			rex_view::addCssFile($this->getAssetsUrl('cache/markitup_profiles.css'));
-			rex_view::addJsFile($this->getAssetsUrl('cache/markitup_profiles.js'));
-		//End - get markitup-profiles
+        rex_view::addCssFile($this->getAssetsUrl('cache/markitup_profiles.css'));
+    	rex_view::addJsFile($this->getAssetsUrl('cache/markitup_profiles.js'));
 	}
 ?>
