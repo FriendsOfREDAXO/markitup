46fe4095b74fa7347b04bcbb2ae5b8044d68e4b1
Thomas Skerbis <thomas.skerbis@mac.com>
2022-09-26 10:52:53 +0200

    require entfernt

--- lib/class.markitup_textile.php
+++ lib/class.markitup_textile.php
@@ -1,10 +1,4 @@
 <?php
-    if (!class_exists('Netcarver\Textile\Parser')) {
-        require_once(rex_path::addon('markitup', 'vendor/php-textile/src/Netcarver/Textile/Parser.php')); //todo: use $this
-        require_once(rex_path::addon('markitup', 'vendor/php-textile/src/Netcarver/Textile/DataBag.php')); //todo: use $this
-        require_once(rex_path::addon('markitup', 'vendor/php-textile/src/Netcarver/Textile/Tag.php')); //todo: use $this
-    }
-
     class markitup_textile extends Netcarver\Textile\Parser
     {
         private static $instances = [];
