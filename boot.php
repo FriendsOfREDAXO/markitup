ec36559cbd0e7d51f84bfd0735145cae65ef1b3f
Norbert Micheel <nm@lautundschoen.de>
2025-12-17 11:48:39 +0100

    change to load late for compatabilty with mform

--- boot.php
+++ boot.php
@@ -4,6 +4,7 @@ namespace FriendsOfRedaxo\MarkItUp;
 
 use rex;
 use rex_addon;
+use rex_extension;
 use rex_i18n;
 use rex_view;
 
@@ -12,26 +13,29 @@ use rex_view;
 if (rex::isBackend()) {
     // Ressourcen einbinden
 
-    rex_view::addJsFile($this->getAssetsUrl('jquery.markitup.js'));
-    rex_view::addJsFile($this->getAssetsUrl('autosize.min.js'));
-    rex_view::addJsFile($this->getAssetsUrl('scripts.js'));
-    rex_view::addCssFile($this->getAssetsUrl('style.css'));
-    if (file_exists($this->getAssetsPath('skin.css'))) {
-        rex_view::addCssFile($this->getAssetsUrl('skin.css'));
-    }
+    rex_extension::register('PACKAGES_INCLUDED', function () {
 
-    $language = array_unique(
-        array_map(
-            static function ($l) { return substr($l, 0, 2); },
-            array_merge([rex_i18n::getLocale()], rex::getProperty('lang_fallback', []), ['--']),
-        ),
-    );
-    foreach ($language as $lang) {
-        $langPath = $this->getAssetsUrl("cache/$lang");
-        if (is_dir($langPath)) {
-            rex_view::addCssFile("$langPath/markitup_profiles.css");
-            rex_view::addJsFile("$langPath/markitup_profiles.js");
-            break;
+        rex_view::addJsFile($this->getAssetsUrl('jquery.markitup.js'));
+        rex_view::addJsFile($this->getAssetsUrl('autosize.min.js'));
+        rex_view::addJsFile($this->getAssetsUrl('scripts.js'));
+        rex_view::addCssFile($this->getAssetsUrl('style.css'));
+        if (file_exists($this->getAssetsPath('skin.css'))) {
+            rex_view::addCssFile($this->getAssetsUrl('skin.css'));
         }
-    }
+
+        $language = array_unique(
+            array_map(
+                static function ($l) { return substr($l, 0, 2); },
+                array_merge([rex_i18n::getLocale()], rex::getProperty('lang_fallback', []), ['--']),
+            ),
+        );
+        foreach ($language as $lang) {
+            $langPath = $this->getAssetsUrl("cache/$lang");
+            if (is_dir($langPath)) {
+                rex_view::addCssFile("$langPath/markitup_profiles.css");
+                rex_view::addJsFile("$langPath/markitup_profiles.js");
+                break;
+            }
+        }
+    }, rex_extension::LATE);
 }
